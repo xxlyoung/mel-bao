4b0e901643deff4ec25d87e6b696f1b6d0921038
Dennis Hou <dennis.hou@disney.com>
2026-01-29 21:44:52 -0800

    Fix Vercel API route configuration

--- api/instagram.js
+++ api/instagram.js
@@ -1,7 +1,7 @@
 // Vercel Serverless Function to fetch Instagram posts
 // Token is stored in INSTAGRAM_ACCESS_TOKEN environment variable
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
